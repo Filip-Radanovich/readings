9c6f63fcc8b9237b79cd21a64908182d8f111e97
Filip Radanović <fradanovic@gmail.com>
2023-05-20 20:34:39 +0200

    prototyping

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,108 @@
-#[derive(Default)]
-pub struct Manifest<'a> {
-    file: (
-        Option<&'a std::path::Path>,
-        Option<std::fs::File>
-    ),
-    entries: Option<Vec<ManifestEntry>>
-}
+use std::io;
+
+use serde::{Deserialize, Serialize};
+
+const VERSION: Version = Version(0, 0, 1);
+
 
 #[derive(Default)]
-pub struct ManifestEntry{
+pub struct Manifest<'a> {
+    path: Option<(&'a std::path::Path, std::fs::File)>,
+    manifest_file: Option<ManifestFile>,
+}
+
+impl <'a> Manifest<'a>{
+    pub fn open(path: &str) -> Self{
+        let path = std::path::Path::new(path);
+        let file = std::fs::File::open(path);
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn manifest_tdd() {
+        Manifest::open("./manifest.venue");
+    }
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct ManifestFile {
+    version: Version,
+    entries: Vec<ManifestEntryFile>,
+}
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct Version(u32, u32, u32);
+
+/// YEAR-MONTH-DAY
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct Date(u32, u32, u32);
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct ManifestEntryFile {
     location: String,
-    date: (u32, u32, u32), // YEAR-MONTH-DAY
+    date: Date, 
     power_meter_state: PowerMeterEntry,
 }
 
-#[derive(Default)]
+#[derive(Default, Debug, Serialize, Deserialize)]
 struct PowerMeterEntry {
     start: Option<PowerMeterState>,
-    end: Option<PowerMeterState>
+    end: Option<PowerMeterState>,
 }
 
+#[derive(Default, Debug, Serialize, Deserialize)]
 struct PowerMeterState {
     code_108: f64,
-    code_104:
+    code_104: f64,
+}
+
+
+
+const OPTIONS: [&str; 4] = [
+    "1. List previous entries",
+    "2. Insert new entry",
+    "3. Change location of manifest file",
+    "4. Create new manifest",
+];
+
+pub fn application(std_in: &io::Stdin, input: &mut String) {
+    print_version();
+    print_intro();
+    print_options();
+    loop {
+        std_in.read_line(input).expect("Error while reading standard input.");
+        let parsed_input = input.trim().parse::<u8>();
+        match parsed_input {
+            Ok(num) => {
+                println!();
+                println!("{}", num);
+                break;
+            }
+            Err(_) => {
+                println!();
+                println!("Only numbers allowed");
+                input.clear();
+                print_options();
+            }
+        }
+    }
+}
+
+pub fn print_version() {
+    println!("Version {}.{}.{}", VERSION.0, VERSION.1, VERSION.2);
+}
+
+pub fn print_intro() {
+    println!("Application for registering power meter readings.");
+    println!("For help press h or ?");
+}
+
+pub fn print_options() {
+    for opt in OPTIONS.into_iter() {
+        println!("{}", opt)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use readings::*;
 use std::io;
 
+
+
 fn main() -> io::Result<()> {
     let std_in = io::stdin();
     let mut input = String::new();
-
-    println!("What do you want to do?");
-    println!(       
-"1. List previous entries
-2. Insert new entry
-3. Change location of manifest file
-4. Create new manifest");
-    std_in.read_line(&mut input)?;
+    application(&std_in, &mut input);
     Ok(())
 }
